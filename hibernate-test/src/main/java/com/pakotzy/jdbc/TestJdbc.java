82a9b0ac2fc75960b9db7cf04c48e62f07e6d1e5
Pavlo Kotelnytskyi <pakotzy@gmail.com>
2018-02-08 12:27:28 +0200

    Started Hibernate
    JDBC connection

---
+++ hibernate-test/src/main/java/com/pakotzy/jdbc/TestJdbc.java
@@ -0,0 +1,20 @@
+package com.pakotzy.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+	private static final String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+	private static final String user = "hbstudent";
+	private static final String pass = "hbstudent";
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Connecting to: " + jdbcUrl);
+			Connection conn = DriverManager.getConnection(jdbcUrl, user, pass);
+			System.out.println("Connection success!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
