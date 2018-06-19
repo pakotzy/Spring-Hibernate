aa28a8a77d51d908e3332208c69b0a3b4995a27e
Pavlo Kotelnytskyi <pakotzy@gmail.com>
2018-06-19 16:24:28 +0300

    Support for precise selection @PathVariable

--- spring-rest-demo/src/main/java/com/pakotzy/springhibernate/rest/controllers/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/pakotzy/springhibernate/rest/controllers/StudentRestController.java
@@ -2,6 +2,7 @@ package com.pakotzy.springhibernate.rest.controllers;
 
 import com.pakotzy.springhibernate.rest.models.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +12,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<>();
+	private List<Student> students = new ArrayList<>();
+
+	public StudentRestController() {
 		students.add(new Student("Pavlo", "Kotelnytskyi"));
 		students.add(new Student("Andrew", "Black"));
 		students.add(new Student("Mario", "Rossi"));
+	}
 
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		return students;
+	}
+
+	@GetMapping("/students/{id}")
+	public Student getStudent(@PathVariable("id") Long id) {
+		return students.get(id.intValue());
 	}
 }
