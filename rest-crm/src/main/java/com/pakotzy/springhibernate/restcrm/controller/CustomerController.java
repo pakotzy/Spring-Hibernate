c078d9cff4fdd7cffe509663f9d96058f1b8b456
Pavlo Kotelnytskyi <pakotzy@gmail.com>
2018-06-20 12:25:56 +0300

    Add and Update customer

--- rest-crm/src/main/java/com/pakotzy/springhibernate/restcrm/controller/CustomerController.java
+++ rest-crm/src/main/java/com/pakotzy/springhibernate/restcrm/controller/CustomerController.java
@@ -3,10 +3,7 @@ package com.pakotzy.springhibernate.restcrm.controller;
 import com.pakotzy.springhibernate.restcrm.entity.Customer;
 import com.pakotzy.springhibernate.restcrm.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,18 @@ public class CustomerController {
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable Integer customerId) {
 		return customerService.getCustomer(customerId);
+	}
+
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		customer.setId(0);
+		customerService.saveCustomer(customer);
+		return customer;
+	}
+
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		customerService.saveCustomer(customer);
+		return customer;
 	}
 }
--- rest-crm/src/main/java/com/pakotzy/springhibernate/restcrm/exception/CustomerNotFoundException.java
+++ rest-crm/src/main/java/com/pakotzy/springhibernate/restcrm/exception/CustomerNotFoundException.java
@@ -5,19 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class CustomerNotFoundException extends RuntimeException {
-	public CustomerNotFoundException() {
-		super();
-	}
 
 	public CustomerNotFoundException(String message) {
 		super(message);
-	}
-
-	public CustomerNotFoundException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public CustomerNotFoundException(Throwable cause) {
-		super(cause);
 	}
 }
