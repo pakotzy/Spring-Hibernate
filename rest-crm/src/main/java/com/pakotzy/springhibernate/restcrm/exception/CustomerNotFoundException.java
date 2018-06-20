d43c9c2c8d89c2011efa54af899d876989b6b25e
Pavlo Kotelnytskyi <pakotzy@gmail.com>
2018-06-20 12:40:17 +0300

    Add exception message by default

--- rest-crm/src/main/java/com/pakotzy/springhibernate/restcrm/exception/CustomerNotFoundException.java
+++ rest-crm/src/main/java/com/pakotzy/springhibernate/restcrm/exception/CustomerNotFoundException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class CustomerNotFoundException extends RuntimeException {
 
-	public CustomerNotFoundException(String message) {
-		super(message);
+	public CustomerNotFoundException() {
+		super("No such customer");
 	}
 }
